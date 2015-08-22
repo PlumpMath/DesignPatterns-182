1d0dbabcc57fa274fa670cb727bb234e54ae1813
unknown <sinhhn@gmail.com>
2015-08-22 11:08:09 +0900

    First commit

---
+++ AbstractFactory/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbtrastFactory
+{
+    class Client
+    {
+        private AbstractProductA absProductA;
+        private AbstractProductB absProductB;
+        public Client(AbstractFactory factory)
+        {
+            absProductA = factory.CreateProductA();
+            absProductB = factory.CreateProductB();
+        }
+        public void Run()
+        {
+            absProductB.Interact(absProductA);
+        }
+    }
+}
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbtrastFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractFactory factory1 = new ConcreteFactory1();
+            Client c1 = new Client(factory1);
+            c1.Run();
+
+            AbstractFactory factory2 = new ConcreteFactory2();
+            Client c2 = new Client(factory2);
+            c2.Run();
+            Console.ReadLine();
+        }
+    }
+}
